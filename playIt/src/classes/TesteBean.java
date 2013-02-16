29f141e5ab416ef5c8deb91bb0d1e151346e887f
diego <diego.tavares.pereira@ccc.ufcg.edu.br>
2013-02-15 22:31:32 -0300

    Interface gráfica - tela de login

---
+++ playIt/src/classes/TesteBean.java
@@ -0,0 +1,42 @@
+package classes;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class TesteBean implements Serializable {
+
+	private static final long serialVersionUID = 5600369132889054255L;
+	
+	private String nome;
+	private String login;
+	private String password;
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
